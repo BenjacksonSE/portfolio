3f705e77620892e1343b8a2e907c8f6ac48b2593
Ben Jackson <benjacksonSE@gmail.com>
2022-03-08 19:51:09 -0500

    Topbar formated

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import classes from './Topbar.module.scss'
+import { FaBars } from 'react-icons/fa'
 
 const Topbar = () => {
   return (
     <div className={classes.topbar}>
       <a href='#' className={classes.logo}>Ben Jackson</a>
-      <div className={classes.toggle}></div>
+      <div className={classes.toggle}>
+        <FaBars />
+      </div>
     </div>
   )
 }
