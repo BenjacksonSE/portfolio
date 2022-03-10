68d75024989136bda3f50ccb947c8cc6bef16551
Ben Jackson <benjacksonSE@gmail.com>
2022-03-10 10:58:43 -0500

    Added explanations to skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -19,32 +19,32 @@ const Skills = () => {
         <div className={classes.content__box}>
           <img src={skill1}/>
           <h2>React</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac congue orci. Morbi non condimentum lorem.</p>
+          <p>The first large Javascript framework I learned.  What I use for the majority of the frontends in personal projects</p>
         </div>
         <div className={classes.content__box}>
           <img src={skill2}/>
           <h2>Ruby on Rails</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac congue orci. Morbi non condimentum lorem.</p>
+          <p>The language I use when createing SQL databases for my backends</p>
         </div>
         <div className={classes.content__box}>
           <img src={skill3}/>
           <h2>CSS and SCSS</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac congue orci. Morbi non condimentum lorem.</p>
+          <p>A Passion of mine is making things tidy and organized thus CSS and I are a great match.</p>
         </div>
         <div className={classes.content__box}>
           <img src={skill4}/>
           <h2>Javascript (ES6+)</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac congue orci. Morbi non condimentum lorem.</p>
+          <p>Even after my bootcamp, I continue to allot multiple hours in my week to practicing and leaning all JS has to offer.  My most proficient skill.</p>
         </div>
         <div className={classes.content__box}>
           <img src={skill5}/>
-          <h2>Node.js</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac congue orci. Morbi non condimentum lorem.</p>
+          <h2>mongoDB</h2>
+          <p>The program I use when creating NoSQL and unstructured databases for my backends</p>
         </div>
         <div className={classes.content__box}>
           <img src={skill6}/>
           <h2>Web Design</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac congue orci. Morbi non condimentum lorem.</p>
+          <p>A more recent endevor, I have become proficient with a design tool, figma, and love creating clean, intuitive UIUX designs.</p>
         </div>
       </div>
     </section>
