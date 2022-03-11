0675ce48f2c2b7badc8ddac2f6535159d110f3da
Ben Jackson <benjacksonSE@gmail.com>
2022-03-11 16:10:02 -0500

    Fixed spelling errors in about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
       <div className={classes.content}>
         <div className={classes.content__text}>
           <p>
-            <br></br><br></br>Hi, my name is Ben Jackson, I am a Full Stack developer with a background in education.  My experience has given me a strong foundation in teamwork, management, and people focused solutions.  Recently, I graduated from Flatiron School’s immersive software development bootcamp.  I have been coding for over 5 years, however, only recently began pursuing it as a full time career, and I couldn’t have made a better choice.<br></br><br></br>I am a well-organized, independent, and an ardent problem solver.  I am an avid runner, fanatical skier, novice cook, and have just started a new hobby or reading and tea drinking.<br></br><br></br>I am interested in the entire stack spectrum and working on ambitious projects with equally as ambitious people.
+            <br></br><br></br>Hi, my name is Ben Jackson, I am a Full Stack developer with a background in education.  My experience has given me a strong foundation in teamwork, management, and people focused solutions.  Recently, I graduated from Flatiron School’s immersive software development bootcamp.  I have been coding for over 5 years, however, only recently began pursuing it as a full time career, and I couldn’t have made a better choice.<br></br><br></br>I am well-organized, independent, and a problem solver.  I am an avid runner, fanatical skier, novice cook, and have just started a new hobby of reading and tea drinking.<br></br><br></br>I am interested in Front End and Full Stack development and working on ambitious projects with equally as ambitious people.
           </p>
         </div>
         <div className={classes.content__imgBox}>
