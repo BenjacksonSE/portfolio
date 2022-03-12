f28405cdcfb1647cf53b925f34b384d4656cf83b
Ben Jackson <benjacksonSE@gmail.com>
2022-03-12 10:21:16 -0500

    Get rid of picture

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,9 +14,9 @@ const About = () => {
             <br></br><br></br>Hi, my name is Ben Jackson, I am a Full Stack developer with a background in education.  My experience has given me a strong foundation in teamwork, management, and people focused solutions.  Recently, I graduated from Flatiron School’s immersive software development bootcamp.  I have been coding for over 5 years, however, only recently began pursuing it as a full time career, and I couldn’t have made a better choice.<br></br><br></br>I am well-organized, independent, and a problem solver.  I am an avid runner, fanatical skier, novice cook, and have just started a new hobby of reading and tea drinking.<br></br><br></br>I am interested in Front End and Full Stack development and working on ambitious projects with equally as ambitious people.
           </p>
         </div>
-        <div className={classes.content__imgBox}>
+        {/* <div className={classes.content__imgBox}>
           <img src='image1' alt='No pic here yet'/>
-        </div>
+        </div> */}
       </div>
     </section>
   )
