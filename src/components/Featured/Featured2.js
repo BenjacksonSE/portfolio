f3e15810b9fd746963dd90dafc12be0f6e01670b
Ben Jackson <benjacksonSE@gmail.com>
2022-03-28 14:39:16 -0400

    Fixed links to blog

--- src/components/Featured/Featured2.js
+++ src/components/Featured/Featured2.js
@@ -11,8 +11,8 @@ const Featured2 = () => {
             <br></br>I’ve enjoyed posting technical blogs during my learning and I wanted to create a site for myself to do so.  <br></br><br></br>This project is built with a React front end and Node.js back end.  On the front end, I used useContext and useRef for state management, on the back end, I used express to handle my server, mutter to handle image storage, crypt to handle password hashing, and mongo DB as my json storage. <br></br><br></br>Technologies used: React, Javascript, HTML, CSS, Node.js 
           </p>
           <div className={classes.linkContainer}>
-            <a href='https://coruscating-manatee-8ac8c9.netlify.app/' target="_blank" rel="noreferrer" className={classes.demoLink}>Demo</a>
-            <a href='https://github.com/BenjacksonSE/sorting-visualizer' target="_blank" rel="noreferrer" className={classes.demoLink}>Github</a>
+            <a href='https://benblogapp.herokuapp.com/' target="_blank" rel="noreferrer" className={classes.demoLink}>Demo</a>
+            <a href='https://github.com/BenjacksonSE/blog-site' target="_blank" rel="noreferrer" className={classes.demoLink}>Github</a>
           </div>
         </div>
         <div className={classes.content__imgBoxL}>
